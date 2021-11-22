d73e65e65768d74fc78b3e23d2f191fffa4cc301
gitzdave <gitzdave@yahoo.com>
2021-11-22 20:24:03 +0000

    1 file modified

--- src/MedicalTreatmentList.js
+++ src/MedicalTreatmentList.js
@@ -8,14 +8,30 @@ function MedicalTreatmentList(props){
   const medicalTreatmentListJ = props.data;
   const medicalTreatmentlist= JSON.parse(medicalTreatmentListJ);
   const ListMedicalTreatmentList = medicalTreatmentlist.map ((medicalTreatment)=>
-<li>
-<MedicalTreatment treatId = {medicalTreatment.treatId} treatCourseId={medicalTreatment.treatCourseId}  name= {medicalTreatment.name} startDate={medicalTreatment.startDate} category={medicalTreatment.category}  type = {medicalTreatment.type}/>
-</li>);
+<table>
+<thead>
+  <th>treatId </th>
+  <th>treatCourseId </th>
+  <th>name </th>
+  <th>startDate </th>
+  <th>type </th>
+  <th>category</th>
+</thead>
+
+  <tbody>
+<td> <MedicalTreatment treatId = {medicalTreatment.treatId} />   </td>
+<td> <MedicalTreatment treatCourseId={medicalTreatment.treatCourseId} /> </td>
+<td><MedicalTreatment name= {medicalTreatment.name}/></td>
+<td><MedicalTreatment startDate={medicalTreatment.startDate}/></td>
+<td><MedicalTreatment category={medicalTreatment.category} /></td>
+<td><MedicalTreatment type = {medicalTreatment.type}/></td>
+</tbody>
+</table>);
 
 return (
-<ul>
+<table>
 {ListMedicalTreatmentList}
-</ul>
+</table>
 
 
 )
