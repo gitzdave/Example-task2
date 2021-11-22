e13d58542545c8bf2207dbfb7ae9788c13cb8491
gitzdave <gitzdave@yahoo.com>
2021-11-22 19:56:18 +0000

    1 file modified

--- src/App.js
+++ src/App.js
@@ -5,19 +5,28 @@ import MedicalTreatmentList from './MedicalTreatmentList';
 
 export default function App() {
 const medicalTreatmentListAsJson =
-'[{ "treatCourseId": "11111" , "treatId": "00002" , "name": "1122", "type": "vaccination", "startDate": "01-10-20" },'
+'[{ "treatCourseId": "11" , "treatId": "01" , "name": "David", "type": "vaccination", "startDate": "01-10-20" },'
 +
-'{ "treatCourseId": "11111" , "treatId": "00002" , "name": "1122", "type": "vaccination", "startDate": "01-10-20" },'+
-'{ "treatCourseId": "11111" , "treatId": "00002" , "name": "1122", "type": "vaccination", "startDate": "01-10-20" },'+
-'{ "treatCourseId": "11111" , "treatId": "00002" , "name": "1122", "type": "vaccination", "startDate": "01-10-20" }]';
+'{ "treatCourseId": "12" , "treatId": "02" , "name": "Mary", "type": "covid", "startDate": "01-10-20" },'+
+'{ "treatCourseId": "13" , "treatId": "03" , "name": "John", "type": "vaccination", "startDate": "01-10-20" },'+
+'{ "treatCourseId": "14" , "treatId": "04" , "name": "Boris", "type": "Vaccination", "startDate": "01-10-20" }]';
 
 const dataString = JSON.parse(medicalTreatmentListAsJson);
+
+
 
   return (
     <div>
       <h1>This is the list of patients: </h1>
      <MedicalTreatmentList  data = {medicalTreatmentListAsJson}/>
 
+     <h1>Search Results: </h1>
+     
+     {dataString.filter(record => record.type =="covid").map(filteredRecord => (
+        <li>
+          {filteredRecord.name}
+        </li>
+      ))}
     </div>
   );
 }
