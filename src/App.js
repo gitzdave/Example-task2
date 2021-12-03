ff9d7a1141b34a7ebcac119e039f09f62d3d2a0f
gitzdave <gitzdave@yahoo.com>
2021-12-03 19:49:05 +0000

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import "./style.css";
 import MedicalTreatmentList from './MedicalTreatmentList';
 import MedicalTreatment from "./MedicalTreatment";
 
-var patientName =" Name is : Dave"
+var treatId = "Treat Id is : 911"
+var patientName =" Name is : Dave  King"
 var category = " Category is: minor"
 var type =" Type is : vaccination"
 var treatCourseId = " Treatcourseid is : 1112"
@@ -13,11 +14,11 @@ var startDate = " StartDate is :  02/12/21"
 
 export default function App() {
 const medicalTreatmentListAsJson =
-'[{ "treatCourseId": "11" , "treatId": "01" , "name": "David", "type": "vaccination", "startDate": "01-10-20" },'
+'[{ "treatCourseId": "11" , "treatId": "01" , "name": "David", "type": "vaccination", "startDate": "01-10-20" , "category":"major"},'
 +
-'{ "treatCourseId": "12" , "treatId": "02" , "name": "Mary", "type": "covid", "startDate": "01-10-20" },'+
-'{ "treatCourseId": "13" , "treatId": "03" , "name": "John", "type": "vaccination", "startDate": "01-10-20" },'+
-'{ "treatCourseId": "14" , "treatId": "04" , "name": "Boris", "type": "Vaccination", "startDate": "01-10-20" }]';
+'{ "treatCourseId": "12" , "treatId": "02" , "name": "Mary", "type": "covid", "startDate": "01-10-20","category":"major" },'+
+'{ "treatCourseId": "13" , "treatId": "03" , "name": "John", "type": "vaccination", "startDate": "01-10-20","category":"major" },'+
+'{ "treatCourseId": "14" , "treatId": "04" , "name": "Boris", "type": "Vaccination", "startDate": "01-10-20" ,"category":"major"}]';
 
 const dataString = JSON.parse(medicalTreatmentListAsJson);
 
@@ -26,6 +27,7 @@ const dataString = JSON.parse(medicalTreatmentListAsJson);
   return (
     <div>
   <h1>  Results of MedicalTreatment </h1>
+  <p> <MedicalTreatment  treatId={treatId}  /> </p>
    <p> <MedicalTreatment  name={patientName}  /> </p>
    <p> <MedicalTreatment  type={type}  /> </p>
    <p> <MedicalTreatment  category={category}  /> </p>
@@ -36,9 +38,9 @@ const dataString = JSON.parse(medicalTreatmentListAsJson);
       <h1> List of patient Records: </h1>
      <MedicalTreatmentList  data = {medicalTreatmentListAsJson}/>
 
-     <h1> Results after filter: </h1>
+     <h1> Results of patient Names after filter: </h1>
      
-     {dataString.filter(record => record.type =="covid").map(filteredRecord => (
+     {dataString.filter(record => record.category =="major").map(filteredRecord => (
         <li>
           {filteredRecord.name}
         </li>
